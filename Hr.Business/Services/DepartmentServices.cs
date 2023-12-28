e8b6585ef97240d4c38966e5e5ddf9f38d9130b8
unknown <aydanmma@code.edu.az>
2023-12-29 02:58:31 +0400

    DepartmentServices and some changes

--- Hr.Business/Services/DepartmentServices.cs
+++ Hr.Business/Services/DepartmentServices.cs
@@ -2,6 +2,8 @@
 using Hr.Business.Utilities.Exeptions;
 using Hr.DataAccess.Contexts;
 using HrManagment.Entities;
+using System;
+using System.Xml.Linq;
 
 namespace Hr.Business.Services;
 
@@ -12,37 +14,26 @@ public class DepartmentServices : IDepartmentServices
     {
         companyServices = new CompanyServices();
     }
-    public void Create(string name, int employeeLimit, int currentEmployeeCount, int companyId, string companyName)
+    public void Create(string name, int employeeLimit, string companyName)
     {
         if (String.IsNullOrEmpty(name)) throw new ArgumentNullException();
         Department? dbDepartment =
             HRDbContext.Departments.Find(c => c.Name.ToLower() == name.ToLower());
         if (dbDepartment is not null)
-            throw new AlreadyExistExcepsion($"{dbDepartment.Name} is already exist");
+            throw new AlreadyExistException($"{dbDepartment.Name} is already exist");
         if (employeeLimit < 4)
             throw new MinCountException("Minimum employee count requirement is 4");
         Company? company = companyServices.FindCompanyByName(companyName);
         if (company is null) throw new NotFoundException($"{companyName} is not exist");
-        Department department = new(name, employeeLimit, currentEmployeeCount, companyId, companyName);
+        Department department = new(name, employeeLimit, company);
         HRDbContext.Departments.Add(department);
     }
-  
-    public void Activate(string name, bool activated)
+    public void Activate(string name)
     {
         if (String.IsNullOrEmpty(name)) throw new ArgumentNullException();
         var isDepartment = HRDbContext.Departments.Find(x => x.Name.ToLower() == name.ToLower());
         if (isDepartment is null) throw new NotFoundException($"{name} department is not found");
         isDepartment.IsActive = true;
-    }
-
-    public Department? GetById(int id)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Department? GetByName(string name)
-    {
-        throw new NotImplementedException();
     }
 
     public void Delete(string name)
@@ -52,18 +43,60 @@ public class DepartmentServices : IDepartmentServices
         if (isDepartment is null) throw new NotFoundException($"{name} department is not found");
         isDepartment.IsActive = false;
     }
-
-    public void ShowAllEmployee(string name)
+    public Department? GetById(int id)
     {
-        throw new NotImplementedException();
+        return HRDbContext.Departments.Find(x => x.Id == id);
+    }
+    public Department? GetByName(string name)
+    {
+        if (String.IsNullOrEmpty(name)) throw new ArgumentNullException();
+        return HRDbContext.Departments.Find(x => x.Name.ToLower() == name.ToLower());
     }
 
     public void ShowAll()
     {
-        throw new NotImplementedException();
+        foreach (var department in HRDbContext.Departments)
+        {
+            if (department.IsActive == true) 
+                Console.WriteLine($"Id: {department.Id}; Department name:{department.Name}");
+        }
+    }
+    public void GetDepartmentEmployee(string name)
+    {
+        foreach (var item in HRDbContext.Employees)
+        {
+            if (item.DepartmentId.Name.ToLower() == name.ToLower())
+            {
+                if (item.IsDelete == false)
+                {
+                    Console.WriteLine($"ID: {item.Id}\n" +
+                                      $"Name: {item.Name}\n" +
+                                      $"Surname: {item.Surname}\n" +
+                                      $"Email: {item.Email}\n" +
+                                      $"Password:{item.Password}\n" +
+                                      $"Salary:{item.Salary}");
+                }
+            }
+        }
+    }
+    public bool IsDepartmentExist()
+    {
+        foreach (var item in HRDbContext.Departments)
+        {
+            if (item is not null && item.IsActive == true)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 
-    
+     public void moveEmployee(int employeeId)
+     {
+        throw new Exception();
+     }
+
+
 }
 
 
